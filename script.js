8c5350a571727631d279f7c4607fea21d6d87fef
BOOBESH776 <cboobesh776@gmail.com>
2026-02-21 11:52:09 +0530

    add remove button functionality and adjust container height in todo list application

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ btn.addEventListener("click", function () {
     let ul = document.createElement("ul");
     let li = document.createElement("li");
     li.textContent = todo;
+    ul.append(li);
+    var output = document.getElementById("output");
+    output.append(ul);
+});
 
-    ul.appendChild(li);
-
-    document.getElementById("output").appendChild(ul);
+remove-btn.addEventListener("click", function () {
+    output = document.getElementById("output");
+    output.innerHTML = "";
 });
