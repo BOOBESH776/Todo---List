c3e65a7a7b18afe5e650758c8ed3f4f915fe041e
BOOBESH776 <cboobesh776@gmail.com>
2026-02-21 12:29:58 +0530

    refactor todo item addition logic for better validation and structure

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 
 btn.addEventListener("click", function () {
     todo = document.getElementById("todo-input").value;
-    let ul = document.createElement("ul");
-    let li = document.createElement("li");
-    li.textContent = todo;
-    ul.append(li);
-    var output = document.getElementById("output");
-    output.append(ul);
+    if(todo === "") {
+        alert("Please enter a todo item.");
+        return;
+    }
+    else{
+        let ul = document.createElement("ul");
+        let li = document.createElement("li");
+        li.textContent = todo;
+        ul.append(li);
+        var output = document.getElementById("output");
+        output.append(ul);
+    }
 });
 
 remove-btn.addEventListener("click", function () {
-    output = document.getElementById("output");
+    var output = document.getElementById("output");
     output.innerHTML = "";
 });
